01cb5475742bdc0ab878ff7a1d5406b2b39d8a21
Aram Karapetyan <aram@dasmeta.com>
2022-12-21 14:03:44 +0400

    refactor: Delete count

--- data.tf
+++ data.tf
@@ -7,21 +7,21 @@ data "aws_iam_openid_connect_provider" "this" {
 }
 
 data "kubectl_file_documents" "clusterrole" {
-  content = file("${path.module}/files/ClusterRole.yaml")
+  content = "${path.module}/files/ClusterRole.yaml"
 }
 
 data "kubectl_file_documents" "clusterrolebinding" {
-  content = file("${path.module}/files/ClusterRoleBinding.yaml")
+  content = "${path.module}/files/ClusterRoleBinding.yaml"
 }
 
 data "kubectl_file_documents" "rolebinding" {
-  content = file("${path.module}/files/RoleBinding.yaml")
+  content = "${path.module}/files/RoleBinding.yaml"
 }
 
 data "kubectl_file_documents" "role" {
-  content = file("${path.module}/files/Role.yaml")
+  content = "${path.module}/files/Role.yaml"
 }
 
 data "kubectl_file_documents" "namespace" {
-  content = file("${path.module}/files/Namespace.yaml")
+  content = "${path.module}/files/Namespace.yaml"
 }
